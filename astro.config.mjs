52c959a40a8dd9c69f21520d0200f708f686dd87
NarayaN Singh <narayan.singh.2571989@gmail.com>
2025-07-14 09:52:39 +0530

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx(), sitemap()],
 });
